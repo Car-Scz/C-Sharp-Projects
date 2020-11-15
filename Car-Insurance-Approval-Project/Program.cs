5900509cb9e202e959ee1a2a71d62f3247c1e72e
Carol Schultz <carol.s.liveproject@gmail.com>
2020-11-14 22:47:10 -0800

    Car Insurance Approval Project
    Three questions are asked to verify is applicant is qualified.  1) age > 15, 2) have DUIs, and 3) no more than 3 speeding tickets.  The decision is then printed.

---
+++ Car-Insurance-Approval-Project/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Insurance_Approval_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Approval Insurance Questionnaire");
+            //  first question
+            Console.WriteLine("\nWhat is your age?");
+            string strAge = Console.ReadLine();
+            int Age = Convert.ToUInt16(strAge);
+            //  second question
+            Console.WriteLine("Have you ever had a DUI (please respond with \"true\" or \"false\" ?");
+            string strDUI = Console.ReadLine();
+            bool isTrue = Convert.ToBoolean(strDUI);
+            // thrid question
+            Console.WriteLine("How many speeding tickets do you have?");
+            string strSpeedTkts = Console.ReadLine();
+            int speedTkts = Convert.ToUInt16(strSpeedTkts);
+
+            // calcuate whether this person meets the criteria of
+            // must be over 15 years, no DUIs, and no more than 3 speeding tickets
+            bool decision = (Age > 15) && (isTrue == false) && (speedTkts <= 3);
+            Console.WriteLine("\nQualified?");
+            Console.WriteLine(decision);
+
+            Console.ReadLine();
+        }
+    }
+}
