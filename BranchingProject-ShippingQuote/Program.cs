6eae6494dd394f4ed0b289206892c40f3398f70f
Carol Schultz <carol.s.liveproject@gmail.com>
2020-11-15 01:32:53 -0800

    Branching Project-Shipping Quote
    A console-based application to get a shipping quote for a package.  There are several error messages that can be generated.

---
+++ BranchingProject-ShippingQuote/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingProject_ShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("\nPlease enter the package weight:");
+            int pkgWeight = Convert.ToInt32(Console.ReadLine());
+            // package to heavy?
+            if (pkgWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("\nPlease enter the package width:");
+                int pkgWidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package height:");
+                int pkgHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length:");
+                int pkgLength = Convert.ToInt32(Console.ReadLine());
+                // Check dimensions
+                if (pkgWidth + pkgHeight + pkgLength > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    // Print out the quote
+                    decimal pkgQuote = ((pkgWidth * pkgHeight * pkgLength) * pkgWeight) / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: " +
+                        (string.Format("{0:C}", pkgQuote)));
+                    Console.WriteLine("Thank you!");
+                }
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ BranchingProject-ShippingQuote/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BranchingProject-ShippingQuote")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("BranchingProject-ShippingQuote")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("966682cb-75a5-417b-8815-c9e9e7c6d78e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
