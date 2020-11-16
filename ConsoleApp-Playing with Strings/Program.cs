433e626581c7bbdb9b39925499523abb95ee8cb8
Carol Schultz <carol.s.liveproject@gmail.com>
2020-11-15 23:59:23 -0800

    Playing with Strings
    This console appp does 3 thrings:
    1)  concatenates 3 strings together
    2)  converts a string to all uppercase
    3)  uses the Stringbuilder method

---
+++ ConsoleApp-Playing with Strings/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Playing_with_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Concatenates three strings.
+            string part1 = "Horas non numero ";
+            string part2 = "nisi serenas ";
+            
+            string sunDialMotto = (part1 + part2 + "(I count only the sunny hours)\n");
+            Console.WriteLine(sunDialMotto);
+
+            //  Converts a string to uppercase.
+            string msg = "This is my C# Program Playing-with-Strings\n";
+            string msgToUppers = msg.ToUpper();
+            Console.WriteLine(msgToUppers);
+
+            //  Creates a Stringbuilder and builds a paragraph of text, one sentence at a time.
+            StringBuilder strbld = new StringBuilder();
+            // Append to StringBuilder.
+            for (int i = 0; i < 10; i++)
+            {
+                strbld.Append(i).Append(" ");
+            }
+            Console.WriteLine(strbld);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
