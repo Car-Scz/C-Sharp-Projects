7ee0522760274ea09ea9854c9beae4463b7f6c87
Carol Schultz <carol.s.liveproject@gmail.com>
2020-11-24 22:41:46 -0800

    Credting a Method Class
    1.  Createe a separate class cs file. which performs three math methods. Each one will take one integer parameter in and return an integer.
    2. In the Main() program, ask the user for a number for the math operations.
    3. Call each method in turn, passing the user input to the method.
    4. Display the returned integer to the screen.

---
+++ ConsoleApp-MathFunctions/ProcessData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class ProcessData
+{
+    public int addNbr(int num1, int num2)
+    {
+        /* local variable declaration */
+        int result;
+
+        result = num1 + num2;
+        return result;
+    }
+    public int subNbr(int num1, int num2)
+    {
+        /* local variable declaration */
+        int result;
+
+        result = num1 - num2;
+        return result;
+    }
+    public int multNbr(int num1, int num2)
+    {
+        /* local variable declaration */
+        int result;
+
+        result = num1 * num2;
+        return result;
+    }
+}
---
+++ ConsoleApp-MathFunctions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /* local variable definition */
+        int a = 200;
+        int ret;
+
+        // Get user input
+        Console.Write("Please pick a number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        // Instantiate an object of the class
+        ProcessData proc = new ProcessData();
+
+        // Printing the start of the "report"...
+        Console.WriteLine("\n==============================");
+        
+        // Calling the addition method
+        ret = proc.addNbr(a, b);
+        Console.WriteLine("\nValues " + a + " plus " + b + " is : {0}", ret);
+        
+        // Calling the subtraction  method
+        ret = proc.subNbr(a, b);
+        Console.WriteLine("\nValues " + a + " subtract " + b + " is : {0}", ret);
+        
+        // Calling the multiplication  method
+        ret = proc.multNbr(a, b);
+        Console.WriteLine("\nValues " + a + " multiply " + b + " is : {0}", ret);
+        
+        // Printing the rest of the "report"...
+        Console.WriteLine("\n==============================");
+
+        Console.WriteLine("Press ENTER to continue. . .");
+        Console.ReadLine();
+    }
+}    
