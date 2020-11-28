977a83aacb4d47991197e7706a59e1f03d2ad45f
Carol Schultz <carol.s.liveproject@gmail.com>
2020-11-27 22:51:35 -0800

    Consol App with Class doing Math
    Created a class with three methods.  The first uses an integer parm and returns an integer.  The second uses a decimal parm and returns an integer.  The third uses a string and returns an integer.
    The Main() program instantiates the class and uses each of the three methods.

---
+++ ConsoleApp-ClassAndMath/DoMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_ClassAndMath
+{
+    class DoMath
+    {
+        private int result;
+        public int ResultX
+        {
+            get
+            {
+                return result;
+            }
+            set
+            {
+                result = 0;
+            }
+        }
+        // this function adds the two values and saves in result  
+        public void MathFunc(int num1, int num2)
+        {
+            result = num1 + num2;
+        }
+        // this function subtracts the the 2nd value from the 1st vavlue and saves in result  
+        public void MathFunc(decimal num1, int num2)
+        {
+            result = Convert.ToInt32(num1) - num2;
+        }
+        // this function multiplies the two numbers saves in result  
+        public void MathFunc(string num1, int num2)
+        {
+            result = Convert.ToInt32(num1) * num2;
+        }
+    }
+}
+       
---
+++ ConsoleApp-ClassAndMath/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_ClassAndMath
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Instantiation #1
+            DoMath classObj = new DoMath();
+            int a = 12;
+            classObj.MathFunc(a, 23);
+            int retVal = classObj.ResultX;
+            Console.WriteLine("\nMatchFunc(int): The return value is {0}", retVal);
+
+            // Instantiation #2
+            DoMath classObj2 = new DoMath();
+            decimal b = 24;
+            classObj2.MathFunc(b, 5);
+            retVal = classObj2.ResultX;
+            Console.WriteLine("\nMatchFunc(decimal): The return value is {0}", retVal);
+
+            // Instantiation #3
+            DoMath classObj3 = new DoMath();
+            string c = "18";
+            classObj3.MathFunc(c, 7);
+            retVal = classObj3.ResultX;
+            Console.WriteLine("\nMatchFunc(string): The return value is {0}", retVal);
+
+            // Wait for user to acknowledge.
+            Console.WriteLine("\nPress Enter to exit. . .");
+            Console.ReadLine();
+        }
+    }
+}
