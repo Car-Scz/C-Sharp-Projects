f229d17ff44146b5f0c921f4d97d466e8403ae8c
Carol Schultz <carol.s.liveproject@gmail.com>
2020-11-28 18:29:31 -0800

    ClassAndMath code fixes
    Fixed code in the MathFunc class:
    1) removed get & set property, 2) changed public void to public int, and 3) added a return statement to each of the 3 methods.
    Fixed Program.cs file:  changed the method call statement.

--- ConsoleApp-ClassAndMath/DoMath.cs
+++ ConsoleApp-ClassAndMath/DoMath.cs
@@ -8,32 +8,24 @@ namespace ConsoleApp_ClassAndMath
 {
     class DoMath
     {
-        private int result;
-        public int ResultX
-        {
-            get
-            {
-                return result;
-            }
-            set
-            {
-                result = 0;
-            }
-        }
+        //public int result { get; set; }
         // this function adds the two values and saves in result  
-        public void MathFunc(int num1, int num2)
+        public int MathFunc(int num1, int num2)
         {
-            result = num1 + num2;
+            int result = num1 + num2;
+            return result;
         }
         // this function subtracts the the 2nd value from the 1st vavlue and saves in result  
-        public void MathFunc(decimal num1, int num2)
+        public int MathFunc(decimal num1, int num2)
         {
-            result = Convert.ToInt32(num1) - num2;
+            int result = Convert.ToInt32(num1) - num2;
+            return result;
         }
         // this function multiplies the two numbers saves in result  
-        public void MathFunc(string num1, int num2)
+        public int MathFunc(string num1, int num2)
         {
-            result = Convert.ToInt32(num1) * num2;
+            int result = Convert.ToInt32(num1) * num2;
+            return result;
         }
     }
 }
--- ConsoleApp-ClassAndMath/Program.cs
+++ ConsoleApp-ClassAndMath/Program.cs
@@ -13,22 +13,19 @@ namespace ConsoleApp_ClassAndMath
             // Instantiation #1
             DoMath classObj = new DoMath();
             int a = 12;
-            classObj.MathFunc(a, 23);
-            int retVal = classObj.ResultX;
+            int retVal = classObj.MathFunc(a, 23);
             Console.WriteLine("\nMatchFunc(int): The return value is {0}", retVal);
 
             // Instantiation #2
             DoMath classObj2 = new DoMath();
             decimal b = 24;
-            classObj2.MathFunc(b, 5);
-            retVal = classObj2.ResultX;
+            retVal = classObj.MathFunc(b, 5);
             Console.WriteLine("\nMatchFunc(decimal): The return value is {0}", retVal);
 
             // Instantiation #3
             DoMath classObj3 = new DoMath();
             string c = "18";
-            classObj3.MathFunc(c, 7);
-            retVal = classObj3.ResultX;
+            retVal = classObj3.MathFunc(c, 7);
             Console.WriteLine("\nMatchFunc(string): The return value is {0}", retVal);
 
             // Wait for user to acknowledge.
