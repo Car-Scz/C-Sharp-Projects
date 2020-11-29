4f4b3365ab49bf43021dbe253b071562e653f7f2
Carol Schultz <carol.s.liveproject@gmail.com>
2020-11-28 22:22:06 -0800

    Math and Optional Parameters
    Console app created with a class that has one required parameter and an optional parameter.  An user must enter the first number but not necessarily the second.  The class takes one or both parameters and returns the value of the math operation within.  The value is then displayed on the screen.

---
+++ ConsoleApp-ClassAndOptionalParm/OptionalParm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_ClassAndOptionalParm
+{
+    // Create a class and then...
+    class OptionalParm
+    {
+        // Create a method that performs a math operation
+        // The method receives two parameter integers and returns an integer result.
+        // Parm 1 is required and parm 2 is optional.
+        public int MathFunc(int num1, int num2=0)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp-ClassAndOptionalParm/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_ClassAndOptionalParm
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Instantiate the class.
+            OptionalParm classObj = new OptionalParm();
+
+            // Ask the user to input two numbers. The first number is required
+            Console.WriteLine("Please enter the first integer: ");
+            int requiredInt = Convert.ToInt32(Console.ReadLine());
+
+            // Thhe second number is not required
+            Console.WriteLine("Please enter the second integer (this integer is not required): ");
+            // get input value, if any
+            string str = Console.ReadLine();
+            // Test for null or empty
+            bool isTrueOrFalse;
+            isTrueOrFalse = str == null || str == string.Empty;
+
+            // optional parm was not entered
+            int retVal;
+            if (isTrueOrFalse == true) 
+            {
+                // Call the method in the class passing in the required parameter
+                retVal = classObj.MathFunc(requiredInt);
+            }
+            else
+            {
+                // Call the method in the class passing both parameters
+                int optionalInt = Convert.ToInt32(str);
+                retVal = classObj.MathFunc(requiredInt, optionalInt);
+            }
+
+            // Display the returned value
+            Console.WriteLine("\nMatchFunc returned the value of {0}", retVal);
+
+            // Wait for user to acknowledge.
+            Console.WriteLine("\nPress Enter to exit. . .");
+            Console.ReadLine();
+        }
+    }
+}
