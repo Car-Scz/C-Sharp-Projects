bfd71ddc034372f06fffb274a9210485170530e2
Carol Schultz <carol.s.liveproject@gmail.com>
2020-11-29 00:11:00 -0800

    Console app using named Parms
    Created a void method in a class that has 2 parameters.  In the method, the first parm has a mth operation performed with it.  The second parm is displayed to the screen.  The first call sends values.  The second call send named values.

---
+++ ConsoleApp-MathAndDisplay/MathFunc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_MathAndDisplay
+{
+    class MathFunc
+    {
+        // 
+        public void AddFunc(int num1, int num2)
+        {
+            int x = num1 * num2;
+            Console.WriteLine("\nThis is the second parameter passed to this method: " + num2);
+        }
+    }
+}
---
+++ ConsoleApp-MathAndDisplay/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_MathAndDisplay
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create an instance of class MathFunc 
+            MathFunc classobj = new MathFunc(); 
+            // sending 2 unnamed parameters to the method
+            classobj.AddFunc(15,25);
+
+            // sending 2 named parameters to the method
+            classobj.AddFunc(num1: 15, num2: 9);
+
+            // Wait for user to acknowledge the results.
+            Console.WriteLine("\nPress Enter to terminate. . .");
+            Console.ReadLine();
+        }
+    }
+}
