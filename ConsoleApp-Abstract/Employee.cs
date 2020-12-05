f912599742c6f4e8715c71c40c464fc81579b45f
Carol Schultz <carol.s.liveproject@gmail.com>
2020-12-04 20:55:56 -0800

    Console app with Abstraction
    1. Created abstract class of Person and has an abstract method called SayName().
    2. Created class Employee which is a child class of Person.
    3.  Implemented the SayName() method within the Employee class.
    4.  Instantiated an Employee object with firstName "Sample" and lastName "Student". cakked the SayName() method on the object.

---
+++ ConsoleApp-Abstract/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Abstract
+{
+    // inherits from Person class
+    class Employee : Person
+    {
+        // properties of this class
+        public int Id { get; set; }
+
+        // Implement the SayName() method inside of the Employee class.
+        public override void SayName() 
+        {
+            Console.WriteLine("\nName: " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ ConsoleApp-Abstract/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object
+            Employee employee = new Employee();
+            // Initialize the  Employee object
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            // Calling the superclass method on the Employee object.
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
