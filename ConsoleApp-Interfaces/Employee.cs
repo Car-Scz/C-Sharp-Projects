a04bbb4540aca5eb055771df6524d1bc19f956e1
Carol Schultz <carol.s.liveproject@gmail.com>
2020-12-04 21:51:11 -0800

    Console app using Interfaces
    Created an interface with one void method. Employee class inheirits the interface and implments the void method. Created an object where the void method was implemented and called the void method. from the program.

---
+++ ConsoleApp-Interfaces/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Interfaces
+{
+    // inherits from Person class
+    class Employee : Person, IQuittable
+    {
+        // properties of this class
+        public int Id { get; set; }
+
+        // Implement the SayName() method inside of the Employee class.
+        public override void SayName()
+        {
+            Console.WriteLine("\nName: " + firstName + " " + lastName);
+        }
+        public void Quit()
+        {
+            // The body of Quit() is provided here
+            Console.WriteLine("I am in the Quit method of the class Employee.  So \"I QUIT!\" ");
+        }
+    }
+}
---
+++ ConsoleApp-Interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object
+            Employee employee = new Employee();
+            // Initialize the  Employee object
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            // Calling the superclass method on the Employee object.
+            employee.SayName();
+            employee.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
