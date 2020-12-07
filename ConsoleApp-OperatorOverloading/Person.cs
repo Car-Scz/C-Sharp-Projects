1ef4137e65cb81103dbdc63d36dc6b55cf44f794
Carol Schultz <carol.s.liveproject@gmail.com>
2020-12-06 16:22:37 -0800

    ConsoleApp for Operator Overloading
    Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.

---
+++ ConsoleApp-OperatorOverloading/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_OperatorOverloading
+{
+    public abstract class Person
+    {
+        // properties of this abstract class
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        // void method with no parameters which prints the person's name
+        public abstract void SayName();
+    }
+}
---
+++ ConsoleApp-OperatorOverloading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_OperatorOverloading
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Instantiate an Employee object
+            Employee employee1 = new Employee();
+            // Initialize the first Employee object
+            employee1.Id = 1;
+            employee1.firstName = "Sample";
+            employee1.lastName = "Student";
+            // Instantiate a second Employee object
+            Employee employee2 = new Employee();
+            // Initialize the second Employee object
+            employee2.Id = 2;
+            employee2.firstName = "Another";
+            employee2.lastName = "Student";
+
+            Console.WriteLine("I am here.");
+            Console.WriteLine(employee1 == employee2);
+            Console.WriteLine(employee1 != employee2);
+
+            // Calling the superclass method on the Employee object.
+            //employee1.SayName();
+            //employee1.Quit();
+            Console.ReadLine();
+        }
+    }
+}
