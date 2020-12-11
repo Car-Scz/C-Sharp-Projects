359ea0c230f8a6206a4bd718a7f2682b0d353df0
Carol Schultz <carol.s.liveproject@gmail.com>
2020-12-10 22:07:59 -0800

    Console app using Struct
    1. Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
    
    2. In the Main() method, create an object of data type Number and assign an amount to it.
    
    3. Print this amount to the console.

---
+++ ConsoleApp-Structs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Structs
+{
+    // Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
+    public struct Number
+    { 
+        public decimal Amount; 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            // create an object of data type Number and assign an amount to it.
+            num1.Amount = 7.99m;
+            // Print this amount to the console.
+            Console.WriteLine($" The Number struct data type Amount has value {num1.Amount}. ");
+            Console.ReadLine();
+        }
+    }
+}
