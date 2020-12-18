0cf0d6af4567e662d96043c7a13c2dc85e111125
Carol Schultz <carol.s.liveproject@gmail.com>
2020-12-17 23:33:25 -0800

    Console app that reads and writes to a text file
    1. Asks the user for a number.
    2. Logs that number to a text file.
    3. Prints the text file back to the user.

---
+++ ConsoleApp-WriteText/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp_WriteText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks the user for a number.
+            Console.WriteLine("Hello, User! Please enter a number");
+            int inputnum = Convert.ToInt32(Console.ReadLine());
+
+            // Logs that number to a text file.
+            string lineout = string.Format("The number is: " + inputnum + "\n");
+            string fileName = "C:\\Users\\carol\\source\\repos\\Logs\\textLog.txt";
+            using (StreamWriter writer = new StreamWriter(fileName, true))
+            {
+                writer.Write(lineout);
+            }
+            // Prints the text file back to the console.
+            string text = "\n" + File.ReadAllText(fileName);
+            Console.WriteLine("Contents of textLog.txt = {0}", text);
+
+            Console.ReadLine();
+        }
+    }
+}
