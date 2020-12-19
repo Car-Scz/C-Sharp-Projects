0d3d91418420ba028fd9cdd25e7ee96833e072df
Carol Schultz <carol.s.liveproject@gmail.com>
2020-12-19 04:04:57 -0800

    Console app using input, date, try/catch
    1. Ask the user for their age.
    2. Display the year the user was born.
    3. Exceptions must be handled using “try/catch.”
    4. Display appropriate error messages if the user enters zero or negative numbers.
    
    5. Display a general message if an exception was caused by anything else.

---
+++ ConsoleApp-DatesAndErrors/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_DatesAndErrors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            var today = DateTime.Today;
+
+            // Exceptions must be handled using “try/catch.”
+            try
+            {
+                // Ask the user for their age.
+                Console.Write("\nWhat is your age?\t");
+                string inputYear = Console.ReadLine();
+                age = int.Parse(inputYear);
+                if (age > 0) 
+                {
+                    int birthYear = today.Year - age;
+                    // Display the year the user was born.
+                    Console.WriteLine("\nYou were born in the year of {0}!", birthYear);
+                } else
+                {
+                    // Display appropriate error messages if the user enters zero or negative numbers.
+                    Console.WriteLine("\nYear cannot be zero or negative!");
+                }
+            }
+            catch (Exception)
+            {
+                // Display a general message if an exception was caused by anything else.
+                Console.WriteLine("\nThe age you entered could not be processed.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
