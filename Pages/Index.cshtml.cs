82ba99b7c61b5bbb8b58f441a3e3c49fb8a57dd7
Carol Schultz <carschul24@gmail.com>
2021-01-14 21:51:17 -0800

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeDisplay.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string Time { get; set; }
+
+        public void OnGet()
+        {
+            Time = DateTime.Now.ToShortTimeString();
+        }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
